709aa070656b63a2d0e07b1c8e09e6e7089efa05
Your Name <addcaa@3152185832@qq.com>
2019-05-17 20:25:00 +0800

    支付宝支付

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -128,13 +128,150 @@ class GoodsController extends Controller
         return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 
+
+
+    public $app_id;
+    public $gate_way;
+    public $notify_url;
+    public $return_url;
+    public $rsaPrivateKeyFilePath;
+    public $aliPubKey;
+    public function __construct(){
+        $this->app_id = env('ALIPAY_APPID');
+        $this->gate_way = 'https://openapi.alipaydev.com/gateway.do';
+        $this->notify_url ='http://www.passport.com/goods/notify';  //回调地址
+        $this->return_url = "http://www.passport.com/goods/aliReturn";
+        $this->rsaPrivateKeyFilePath = storage_path('app/key/private.pem');   //应用私钥
+        $this->aliPubKey = storage_path('app/key/public.pem'); //支付宝公钥
+    }
+
     /**
      * 提交订单
      */
     public function submitorder(){
-        $u_id=$_GET['u_id'];
+        $goods_price=$_GET['goods_price'];
+        //echo $goods_price;
         $goods_price=$_GET['goods_price'];
         $on_order=$_GET['on_order'];
-        dd($u_id,$goods_price,$on_order);
+        $info=DB::table('order')->where(['on_order'=>$on_order])->first();
+//        dd($u_id,$goods_price,$on_order);
+        //判断订单是否已被支付
+        if($info->pay_time>0){
+            die("订单已支付，请勿重复支付");
+        }
+        //判断订单是否已被删除
+        if($info->is_delete==1){
+            die("订单已被删除，无法支付");
+        }
+        $oid=$info->oid;
+        //业务参数
+        $bizcont = [
+            'subject'           => 'Lening-Order: ' .$oid, //订单标提
+            'out_trade_no'      => $on_order,       //订单号
+            'total_amount'      => $goods_price,    //订单价格
+            'product_code'      => 'QUICK_WAP_WAY',
+        ];
+        //公共参数
+        $data = [
+            'app_id'   => $this->app_id,
+            'method'   => 'alipay.trade.wap.pay',
+            'format'   => 'JSON',
+            'charset'   => 'utf-8',
+            'sign_type'   => 'RSA2',
+            'timestamp'   => date('Y-m-d H:i:s'),
+            'version'   => '1.0',
+            'notify_url'   => $this->notify_url,        //异步通知地址
+            'return_url'   => $this->return_url,        // 同步通知地址
+            'biz_content'   => json_encode($bizcont),
+        ];
+
+        //签名
+        $sign = $this->rsaSign($data);
+        $data['sign'] = $sign;
+        $param_str = '?';
+        foreach($data as $k=>$v){
+            $param_str .= $k.'='.urlencode($v) . '&';
+        }
+        $url = rtrim($param_str,'&');
+        $url = $this->gate_way . $url;
+        return redirect($url, 302);
+//        header("Location:$url");       // 重定向到支付宝支付页面
+    }
+
+
+    public function rsaSign($params) {
+        return $this->sign($this->getSignContent($params));
+    }
+    protected function sign($data) {
+        $priKey = file_get_contents($this->rsaPrivateKeyFilePath);
+        $res = openssl_get_privatekey($priKey);
+        ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
+        openssl_sign($data, $sign, $res, OPENSSL_ALGO_SHA256);
+        if(!$this->checkEmpty($this->rsaPrivateKeyFilePath)){
+            openssl_free_key($res);
+        }
+        $sign = base64_encode($sign);
+        return $sign;
+    }
+    public function getSignContent($params) {
+        ksort($params);
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === $this->checkEmpty($v) && "@" != substr($v, 0, 1)) {
+                // 转换成目标字符集
+                $v = $this->characet($v, 'UTF-8');
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+    protected function checkEmpty($value) {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+        return false;
+    }
+    /**
+     * 转换字符集编码
+     * @param $data
+     * @param $targetCharset
+     * @return string
+     */
+    function characet($data, $targetCharset) {
+        if (!empty($data)) {
+            $fileType = 'UTF-8';
+            if (strcasecmp($fileType, $targetCharset) != 0) {
+                $data = mb_convert_encoding($data, $targetCharset, $fileType);
+            }
+        }
+        return $data;
+    }
+    /**
+     * 支付宝异步通知
+     */
+    public function notify()
+    {
+        $p = json_encode($_POST);
+        $log_str = "\n>>>>>> " .date('Y-m-d H:i:s') . ' '.$p . " \n";
+        file_put_contents('logs/alipay_notify',$log_str,FILE_APPEND);
+        echo 'success';
+        //TODO 验签 更新订单状态
+    }
+    /**
+     * 支付宝同步通知
+     */
+    public function aliReturn()
+    {
+        echo '<pre>';print_r($_GET);echo '</pre>';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,17 @@ Route::get('/', function () {
 });
 
 Route::post('api/reg','User\UserController@reg' );//注册
-
 Route::post('api/log','User\UserController@log' );//登录
-
-Route::get('api/index','User\UserController@index');//登录
-
-
+Route::get('api/index','User\UserController@index');//
 Route::get('goods/cart','Goods\GoodsController@cart');//商品详情
-
 Route::get('goods/car','Goods\GoodsController@car');//加入购物车
-
 Route::get('goods/shopping','Goods\GoodsController@shopping');//商品购物车
-
 Route::get('goods/buy','Goods\GoodsController@buy');//订单
-
 Route::get('goods/drop','Goods\GoodsController@drop');//订单列表
-
 Route::get('goods/submitorder','Goods\GoodsController@submitorder');//订单列表
+
+
+//支付宝异步回调
+Route::get('goods/notify','Goods\GoodsController@notify');
+
+Route::get('goods/aliReturn','Goods\GoodsController@aliReturn');
